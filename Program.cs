f7151977bdd7c5a3bb0a25a25770cf5b35ad9c79
Ivan Dzherikhov <jodsonjod@gmail.com>
2019-11-29 10:03:34 +0200

    Додано підрахунок суми чисел і виведення результату

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 5;
+            int y = 10;
+            Console.WriteLine("5 + 10 = {0}", x + y);
             Console.ReadKey();
         }
     }
